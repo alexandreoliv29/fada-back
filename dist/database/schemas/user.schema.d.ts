8c21ba6242432c5f016ab09bffd92b7dea105a3d
alexandre <alexandre.foj@gmail.com>
2023-01-10 11:53:21 -0300

    removing dist

--- dist/database/schemas/user.schema.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="mongoose/types/PipelineStage" />
-/// <reference types="mongoose" />
-export declare type UserDocument = User & Document;
-export declare class User {
-    username: string;
-    email: string;
-    password: string;
-}
-export declare const UserSchema: import("mongoose").Schema<User, import("mongoose").Model<User, any, any, any>, any, any>;
--- dist/dtos/wine.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WineDto = void 0;
-class WineDto {
-}
-exports.WineDto = WineDto;
-//# sourceMappingURL=wine.dto.js.map
