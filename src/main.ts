0a885d7bacc50b77dbc489a958459eef1098571a
alexandre <alexandre.foj@gmail.com>
2023-01-10 10:49:39 -0300

    changing main

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  const options = {
+    origin: '*',
+    allowedHeaders: '*',
+    methods: "*",
+    credentials: true
+  };
+  app.enableCors(options);
+  await app.listen(process.env.PORT || 3002);
 }
 bootstrap();
 
